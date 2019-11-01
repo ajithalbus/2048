b8f073aa719379bdb57c087e761c0d29f4530bae
Ajith Kumar <ajith.km@flipkart.com>
2019-11-01 22:40:55 +0530

    v0

--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
-#include <iostream>
 #include "utils.h"
 #include "game.h"
-using namespace std;
 
 int main(){
     initSeed();
-    /*State state;
-    state.init();
-    state.render();
-    cout<<"\n";
-    //for(int i =0 ;i<10;i++)
-        state.moveLeft();
-        state.render();
-        state.moveUp();
-        state.render();
-        state.moveRight();
-        
-        state.render();
-        state.moveDown();
-        state.render();*/
-
     Game game;
     game.play();
 }
